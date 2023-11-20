2e31e4563a71538951ef3e4634c561471127d58e
kunhwi <73335725+kunhwi04@users.noreply.github.com>
2023-11-20 18:33:39 +0900

    index.c

--- index.c
+++ index.c
@@ -5,24 +5,24 @@
 
 void clearScreen() {
     system("cls");
-} //콘솔창 리셋
+}
 
 void initializeArray(int arr[], int size) {
     for (int i = 0; i < size; ++i) {
         arr[i] = 0;
     }
-} //"1" 움직이기
+}
 
 void printArray(int arr[], int size, int position) {
     for (int i = 0; i < size; ++i) {
         printf("%c ", (i == position) ? '1' : '0');
     }
     printf("\n");
-} //"1" 움직이기
+}
 
 int main() {
     int array[SIZE];
-    int position = 0; // 현재 위치
+    int position = 0;
 
     initializeArray(array, SIZE);
     array[position] = 1;
@@ -37,9 +37,9 @@ int main() {
 
         if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
             position = (position < SIZE - 1) ? (position + 1) : position;
-        } // 방향키 움직이기
+        }
 
-        Sleep(100); // 딜레이
+        Sleep(100);
     }
 
     return 0;
